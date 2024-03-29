9b37335af5a56f863cf815134c149838ede128a0
Lars van der Wijden <sral8991@gmail.com>
2024-03-29 15:26:29 +0100

    some updates from a while back

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # pbThread
 Gives highscores for pb's in discord
+
+## startup testenv
+```bash
+ngrok http 3000
+nodemon app.js
+```
+
--- commands.js
+++ commands.js
@@ -1,45 +1,106 @@
 import 'dotenv/config';
-import { getRPSChoices } from './game.js';
 import { capitalize, InstallGlobalCommands } from './utils.js';
 
-// Get the game choices from game.js
-function createCommandChoices() {
-  const choices = getRPSChoices();
-  const commandChoices = [];
+// Help command for showing help dialogue
+const HELP_COMMAND = {
+	name: 'Help',
+	description: 'Get help with this bot or one of its commands',
+	options: [
+	{
+		type: 3,
+		name: 'command',
+		description: 'The command you want help with',
+		choices: getAllCommands(),
+	},
+	type: 1,
+};
 
-  for (let choice of choices) {
-    commandChoices.push({
-      name: capitalize(choice),
-      value: choice.toLowerCase(),
-    });
-  }
+// submit new pb for a boss
+const SUBMIT_COMMAND = {
+	name: 'submit',
+	description: 'Submit a new time for a boss',
+	options: [
+		{
+			type: 3,
+			name: 'boss',
+			description: 'The boss you got a new pb for',
+			required: true,
+			// TODO: add db connection, for now it's only what's hardcoded here
+			choices: [
+			{
+				name: "test",
+				value: "test_test"
+			},
+			{
+				name: "Obor",
+				value: "boss_obor"
+			},
+			{
+				name: "Sarachnis",
+				value: "boss_sarachnis"
+			}
+			],
+		},
+		{
+			type: 4,
+			name: 'time',
+			description: 'Give in this format: hh:mm:ss.ms',
+			required: true,
+		},
+	],
+	type: 1,
+};
 
-  return commandChoices;
+// highscores to get a top 3 for the requested boss
+const HIGHSCORES_COMMAND = {
+	name: 'highscores',
+	description: 'Get highscores for a boss',
+	options: [
+		{
+			type: 3,
+			name: 'boss',
+			description: 'The boss you want to know the highscores for'
+			required: true,
+			// TODO: add db connection, for now it's only what's hardcoded here
+			choices: [
+				{
+					name: "test",
+					value: "test_test"
+				},
+				{
+					name: "Obor",
+					value: "boss_obor"
+				},
+				{
+					name: "Sarachnis",
+					value: "boss_sarachnis"
+				}
+			],
+		},
+		{
+			type: 4,
+			name: 'nr',
+			description: 'The number of entries you want to show. The default is 3.'
+			required: false,
+		},
+	],
+	type: 1,
+};
+
+
+const ALL_COMMANDS = [HELP_COMMAND, SUBMIT_COMMAND, HIGHSCORES_COMMAND];
+
+// parse ALL_COMMANDS into options
+function getAllCommands() {
+	const listAllCommands = [];
+
+	for (let comm of ALL_COMMANDS) {
+		listAllCommands.push({
+			name: comm,
+			value: "comm_"+comm
+		});
+	}
+	return listAllCommands;
 }
-
-// Simple test command
-const TEST_COMMAND = {
-  name: 'test',
-  description: 'Basic command',
-  type: 1,
-};
-
-// Command containing options
-const CHALLENGE_COMMAND = {
-  name: 'challenge',
-  description: 'Challenge to a match of rock paper scissors',
-  options: [
-    {
-      type: 3,
-      name: 'object',
-      description: 'Pick your object',
-      required: true,
-      choices: createCommandChoices(),
-    },
-  ],
-  type: 1,
-};
-
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
